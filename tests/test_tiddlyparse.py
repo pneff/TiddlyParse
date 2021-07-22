90fe29f6ce254337a79c8414e4db6f111eccec1b
Patrice Neff <mail@patrice.ch>
2021-07-22 13:04:30 +0200

    Get all the tiddlers

--- tests/test_tiddlyparse.py
+++ tests/test_tiddlyparse.py
@@ -1,24 +1,41 @@
 from pathlib import Path
 
+from pytest import fixture
 from tiddlyparse import __version__, parse
 from tiddlyparse.parser import FileFormat
 
 FIXTURES = Path(__file__).parent / "fixtures"
 
 
+@fixture
+def div_wiki():
+    fixture_name = FIXTURES / "empty-5.1.23.html"
+    wiki = parse(file=fixture_name)
+    yield wiki
+
+
+@fixture
+def json_wiki():
+    fixture_name = FIXTURES / "empty-5.2.0.html"
+    wiki = parse(file=fixture_name)
+    yield wiki
+
+
 def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_parse_div_format():
-    fixture_name = FIXTURES / "empty-5.1.23.html"
-    wiki = parse(file=fixture_name)
-    assert wiki.fileformat == FileFormat.DIV
-    # assert len(wiki) == 2103
+def test_parse_div_format(div_wiki):
+    assert div_wiki.fileformat == FileFormat.DIV
 
 
-def test_parse_json_format():
-    fixture_name = FIXTURES / "empty-5.2.0.html"
-    wiki = parse(file=fixture_name)
-    assert wiki.fileformat == FileFormat.JSON
-    # assert len(wiki) == 2130
+def test_parse_div_length(div_wiki):
+    assert len(div_wiki) == 4
+
+
+def test_parse_json_format(json_wiki):
+    assert json_wiki.fileformat == FileFormat.JSON
+
+
+def test_parse_json_length(json_wiki):
+    assert len(json_wiki) == 7
--- tiddlyparse/parser.py
+++ tiddlyparse/parser.py
@@ -1,10 +1,12 @@
+import json
 from abc import ABC, abstractmethod
 from enum import Enum
+from json.decoder import JSONDecodeError
 from pathlib import Path
-from typing import Union
+from typing import Mapping, Sequence, Union
 
 from bs4 import BeautifulSoup
-from bs4.element import NavigableString, Tag
+from bs4.element import NavigableString, PageElement, Tag
 
 
 class FileFormat(Enum):
@@ -18,6 +20,20 @@ class UnknownTiddlywikiFormatError(ValueError):
     pass
 
 
+class Tiddler:
+    pass
+
+
+class DivTiddler(Tiddler):
+    def __init__(self, el: PageElement):
+        pass
+
+
+class JsonTiddler(Tiddler):
+    def __init__(self, tiddler: Mapping[str, str]):
+        pass
+
+
 class TiddlyParser(ABC):
     filename: Path
     fileformat: FileFormat
@@ -29,41 +45,86 @@ class TiddlyParser(ABC):
     def is_format(cls, file: Path, soup: BeautifulSoup) -> bool:
         pass
 
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
 
 class JsonTiddlyParser(TiddlyParser):
     fileformat: FileFormat = FileFormat.JSON
+
+    _root: Tag
+    _tiddlers: Sequence[Tiddler]
 
     def __init__(self, file: Path, soup: BeautifulSoup):
         self.fileformat = FileFormat.JSON
         self.filename = file
         self._soup = soup
-        self._root = self._get_container(soup)
+        root = self._get_container(soup)
+        if not isinstance(root, Tag):
+            raise UnknownTiddlywikiFormatError("Could not find root element.")
+        self._root = root
+        self._tiddlers = self._load_tiddlers()
 
     @classmethod
     def is_format(cls, file: Path, soup: BeautifulSoup) -> bool:
         return bool(cls._get_container(soup))
+
+    def __len__(self) -> int:
+        return len(self._tiddlers)
 
     @staticmethod
     def _get_container(soup: BeautifulSoup) -> Union[Tag, NavigableString, None]:
         return soup.find("script", class_="tiddlywiki-tiddler-store")
 
+    def _load_tiddlers(self) -> Sequence[Tiddler]:
+        tiddlers = []
+        text = self._root.string
+        if not text:
+            raise UnknownTiddlywikiFormatError("No tiddler content found.")
+        try:
+            raw_tiddlers = json.loads(text)
+        except JSONDecodeError:
+            raise UnknownTiddlywikiFormatError(
+                f"Could not parse the JSON tiddler with the text {text[0:100]!r}"
+            )
+        for tiddler in raw_tiddlers:
+            tiddlers.append(JsonTiddler(tiddler))
+        return tiddlers
+
 
 class DivTiddlyParser(TiddlyParser):
     fileformat: FileFormat = FileFormat.DIV
+
+    _root: Tag
+    _tiddlers: Sequence[Tiddler]
 
     def __init__(self, file: Path, soup: BeautifulSoup):
         self.fileformat = FileFormat.DIV
         self.filename = file
         self._soup = soup
-        self._root = self._get_container(soup)
+        root = self._get_container(soup)
+        if not isinstance(root, Tag):
+            raise UnknownTiddlywikiFormatError("Could not find root element.")
+        self._root = root
+        self._tiddlers = self._load_tiddlers()
 
     @classmethod
     def is_format(cls, file: Path, soup: BeautifulSoup) -> bool:
         return bool(cls._get_container(soup))
 
+    def __len__(self) -> int:
+        return len(self._tiddlers)
+
     @staticmethod
     def _get_container(soup: BeautifulSoup) -> Union[Tag, NavigableString, None]:
         return soup.find("div", id="storeArea")
+
+    def _load_tiddlers(self) -> Sequence[Tiddler]:
+        tiddlers = []
+        for container in self._root("div"):
+            tiddlers.append(DivTiddler(container))
+        return tiddlers
 
 
 def parse(file: Path) -> TiddlyParser:
